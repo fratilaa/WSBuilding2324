858709a61a017b5e56703e97f08c1350a625b68d
Antu <fratilaa99@gmail.com>
2024-03-14 17:21:30 +0200

    web

---
+++ curs1/js/carousel.js
@@ -0,0 +1,9 @@
+'use strict'
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: true,
+  pause: 'hover', //false
+  keyboard:true,
+  touch: true
+})
---
+++ curs1/js/scrollspy.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     var myScrollSpy = new bootstrap.ScrollSpy(document.body, {
+//         target: "#myNavbar"
+//     })
+// });
+
+$(document).ready(function () {
+    $("body").scrollspy({
+        target: "#myNavbar"
+    })
+
+});
